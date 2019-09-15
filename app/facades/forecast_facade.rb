fe639e035bc4e74adf88dbbb29601fad13dca983
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-09-15 15:32:16 -0600

    Added DarkSky Service, Geo Serializer not need

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -10,9 +10,18 @@ class ForecastFacade #refactor with SimpleDelegator
     Coordinate.new(coords["lat"], coords['lng'])
   end
 
+  def forecast
+    grab_forecast
+    require 'pry'; binding.pry
+  end
+
 private
 
   def grab_geocode
     GeocodingApi.new(@location).make_call
+  end
+
+  def grab_forecast
+    DarkskyApi.new(geo_coordinates.latitude, geo_coordinates.longitude).make_call
   end
 end
