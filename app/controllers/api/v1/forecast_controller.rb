ec8f880d442d4ea89c6ab1b96b95b7f30718e905
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-09-15 14:59:24 -0600

    finishes geocoding

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,4 +1,8 @@
-class ForecastController < ApplicationController
+class Api::V1::ForecastController < ApplicationController
 
+  def index
+    render locals: {facade: ForecastFacade.new(params[:location]).geo_coordinates}
+    require 'pry'; binding.pry
+  end
 
 end
--- app/services/geocoding_api.rb
+++ app/services/geocoding_api.rb
@@ -12,9 +12,9 @@ class GeocodingApi
   private
 
   def conn
-    Faraday.new(url: 'https://maps.googleapis.com') do |f|
-      faraday.params['key'] = ENV['GOOGLE_KEY']
-      faraday.adapter Faraday.default_adapter
+    conn = Faraday.new(url: 'https://maps.googleapis.com') do |f|
+      f.params['key'] = ENV['GOOGLE_KEY']
+      f.adapter Faraday.default_adapter
     end
   end
 
