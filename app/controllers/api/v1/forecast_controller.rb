e27bc0bab37fa392da8b752185df722f406a96d2
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-09-16 09:32:38 -0600

    Adding Gif route and controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::ForecastController < ApplicationController
 
   def index
-    forecast = ForecastFacade.new(params[:location]).forecast
+    forecast = ForecastFacade.new(location_params).forecast
     render json: ForecastSerializer.new(forecast)
-    require 'pry'; binding.pry
+  end
+
+  private
+
+  def location_params
+    params.permit(:location)
   end
 
 end
